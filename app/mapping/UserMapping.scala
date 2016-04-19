d1554036a51a58508aaf20de20a7e354fe99fa78
Marek Kadek <kadek.marek@gmail.com>
2016-04-20 09:29:25 +1200

    init skeleton

--- app/mapping/UserMapping.scala
+++ app/mapping/UserMapping.scala
@@ -1,9 +1,31 @@
 package mapping
 
+import java.util.UUID
+
+import com.mohiva.play.silhouette.api.LoginInfo
 import model.core.User
 import slick.driver.H2Driver.api._
 
-/*
-sealed class UserMapping(tag: Tag) extends Table[User] {
+sealed class UserMapping(tag: Tag) extends Table[User](tag, "users") {
+  def uuid: Rep[String] = column[String]("uuid", O.PrimaryKey)
 
-}*/
+  def * = ???
+}
+
+sealed class LoginInfoMapping(tag: Tag) extends Table[LoginInfo](tag, "logininfo") {
+  def * = ???
+}
+
+
+final case class UserToLoginInfo(userUuid: String, loginInfoId: Long)
+sealed class UserToLoginInfoMapping(tag: Tag) extends Table[UserToLoginInfo](tag, "user_to_logininfo") {
+  def * = ???
+}
+
+final case class PasswordInfo(loginInfoId: Long,
+                                hasher: String,
+                                password: String,
+                                salt: Option[String])
+class PasswordInfoMapping(tag: Tag) extends Table[PasswordInfo](tag, "passwordinfo") {
+  def * = ???
+}
--- app/module/ServiceModule.scala
+++ app/module/ServiceModule.scala
@@ -2,15 +2,20 @@ package module
 
 import com.google.inject.AbstractModule
 import net.codingwell.scalaguice.ScalaModule
-import service.{Hasher, UserTokenService, UserService}
-import service.impl.{InMemoryUserTokenServiceImpl, InMemoryUserServiceImpl, Sha1HasherImpl}
+import service.{Hasher, UserService, UserTokenService}
+import service.impl._
 
 sealed class ServiceModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
     bind[Hasher].to[Sha1HasherImpl]
 
-    //bind[UserService].to[UserServiceImpl]
-    bind[UserService].to[InMemoryUserServiceImpl].asEagerSingleton
-    bind[UserTokenService].to[InMemoryUserTokenServiceImpl].asEagerSingleton
+    bind[InitDb].asEagerSingleton
+    bind[UserService].to[UserServiceImpl]
+    bind[UserTokenService].to[UserTokenServiceImpl]
   }
 }
+
+class InitDb {
+  println("creating db")
+}
+
