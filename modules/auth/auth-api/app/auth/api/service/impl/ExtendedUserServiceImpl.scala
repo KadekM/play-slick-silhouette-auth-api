f5f96b37935fc25361ad695d7befd15f7447bf87
Mike Slinn <mslinn@micronauticsresearch.com>
2016-05-15 11:39:33 -0700

    Suppressed over-eager Option2Iterable wart accusation

--- modules/auth/auth-api/app/auth/api/service/impl/ExtendedUserServiceImpl.scala
+++ modules/auth/auth-api/app/auth/api/service/impl/ExtendedUserServiceImpl.scala
@@ -19,24 +19,26 @@ class ExtendedUserServiceImpl(dbConfigProvider: AuthDatabaseConfigProvider)(
 
   import driver.api._
 
-  // TODO: temporary implementation, fix reduntant roundtrip and boilerplate
+  // TODO: temporary implementation, fix redundant roundtrip and boilerplate
+  @SuppressWarnings(Array("org.brianmckenna.wartremover.warts.Option2Iterable"))
   override def update(userUuid: UUID, update: UpdateUser): Future[Boolean] = {
-
     val findUser = usersQuery.filter(_.uuid === userUuid)
 
     val firstNameUpdate = update.firstName.map { v ⇒
       findUser.map(u ⇒ u.firstName).update(v)
     }
+
     val lastNameUpdate = update.lastName.map { v ⇒
       findUser.map(u ⇒ u.lastName).update(v)
     }
+
     val emailUpdate = update.email.map { v ⇒
       throw new NotImplementedError("updating email!, what should happen?")
       findUser.map(u ⇒ u.email).update(v)
     }
 
     Seq(firstNameUpdate, lastNameUpdate, emailUpdate)
-      .flatMap(x ⇒ x.toIterable)
+      .flatten
       .map(db.run(_).map(_ ⇒ true))
       .head
   }
